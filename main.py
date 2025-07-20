0a650603080fcdb329f143e1f9cfa047ba16d5da
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-20 05:22:42 -0700

    development of the form

--- main.py
+++ main.py
@@ -20,12 +20,19 @@ namevalue = StringVar
 phonevalue =StringVar
 gendervalue = StringVar
 emergencyvalue = StringVar
-payementmood = StringVar
+payementmoodvalue = StringVar
+checkvalue = IntVar
 
 nameentry = Entry(root, textvariable=namevalue)
 phoneentry = Entry(root, textvariable=phonevalue)
 genderentry = Entry(root, textvariable=gendervalue)
 emergencyentry = Entry(root, textvariable=emergencyvalue)
 payementmoodentry = Entry(root, textvariable=payementmoodvalue)
+
+nameentry.grid(row=1, column=3)
+phoneentry.grid(row=2, column=3)
+genderentry.grid(row=3, column=3)
+emergencyentry.grid(row=4, column=3)
+payementmoodentry.grid(row=5, column=3)
 
 root.mainloop()
