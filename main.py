9cce585a847120b82fee6409a82b856ff66da63c
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-20 05:17:59 -0700

    methods

--- main.py
+++ main.py
@@ -16,4 +16,16 @@ gender.grid(row=3, column=2)
 emergency.grid(row=4, column=2)
 payementmood.grid(row=5, column=2)
 
+namevalue = StringVar
+phonevalue =StringVar
+gendervalue = StringVar
+emergencyvalue = StringVar
+payementmood = StringVar
+
+nameentry = Entry(root, textvariable=namevalue)
+phoneentry = Entry(root, textvariable=phonevalue)
+genderentry = Entry(root, textvariable=gendervalue)
+emergencyentry = Entry(root, textvariable=emergencyvalue)
+payementmoodentry = Entry(root, textvariable=payementmoodvalue)
+
 root.mainloop()
