e8813a1cf78ee3b07a6a04e56a0d1773f009649b
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-20 04:44:41 -0700

    first push about the form

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+root = Tk()
+root.geometry("500x350")
+
+Label(root, text= "Python Registration Form" , font="arial 15 bold").grid(row=0 , column=3)
+
+name = Label(root, text="Name")
+phone = Label(root, text="phone")
+gender = Label(root, text="gender")
+emergency = Label(root, text="emergency")
+payementmood = Label(root, text="payementmood")
+
+
+
+root.mainloop()
